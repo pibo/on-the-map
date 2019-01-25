92e1cff55589da8d02fa2b6a39a1e5724f8f1b40
Felipe Ribeiro <feliperibeiro@me.com>
2019-01-25 15:49:50 -0200

    Acredito ter corrigido um crash frequente ao recriar clusters no mapa

--- On The Map/Views/StudentAnnotation.swift
+++ On The Map/Views/StudentAnnotation.swift
@@ -8,27 +8,28 @@
 
 import MapKit
 
-class StudentAnnotation: MKPointAnnotation {
+class StudentAnnotation: NSObject, MKAnnotation {
     
     // MARK: Properties
     
+    let coordinate: CLLocationCoordinate2D
+    let title: String?
+    let subtitle: String?
     let studentLocation: StudentLocation
     let markColor: UIColor
     
     var memberAnnotations: [MKAnnotation] = []
     
     // MARK: Initializer
-    
+
     init(studentLocation: StudentLocation, markColor: UIColor) {
         self.studentLocation = studentLocation
         self.markColor = markColor
-        
-        super.init()
-        
+
         let latitude = CLLocationDegrees(studentLocation.latitude!)
         let longitude = CLLocationDegrees(studentLocation.longitude!)
         coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        
+
         title = studentLocation.fullName
         subtitle = studentLocation.mediaURL
     }
